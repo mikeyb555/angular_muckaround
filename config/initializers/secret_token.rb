65d007ea20c9d307c538f677bce84e9bd94f025e
Michae <shrapnelbikes@gmail.com>
2013-06-29 14:36:18 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CocktailMaker::Application.config.secret_token = 'a63a90114a0b1769fdc37c46ba6d4c167fa4dfe142c1dc8a743ec610c074691eef08ddc39cdc167a116210157274373b53c685257d6c4fe500609bd44a4c2a0d'
